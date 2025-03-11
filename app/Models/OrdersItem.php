a99e774d05846c42b26ab4c97c2a54b4ad8c3197
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-03-11 13:19:33 +0100

    * Creating ordersItem table and model

---
+++ app/Models/OrdersItem.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrdersItem extends Model
+{
+    protected $fillable = ['order_id', 'product_id', 'amount', 'price'];
+}
